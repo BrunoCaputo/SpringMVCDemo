c90a2c0c38efb3c04ca12d1231d778738116d57f
Bruno Caputo <bruno.caputo@leucotron.com.br>
2019-07-17 16:34:31 -0300

    Added a CRUD to Cargo

---
+++ demo-mvc/src/main/java/com/bruno/curso/boot/web/conversor/StringToDepartamentoConversor.java
@@ -0,0 +1,25 @@
+package com.bruno.curso.boot.web.conversor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import com.bruno.curso.boot.domain.Departamento;
+import com.bruno.curso.boot.service.DepartamentoService;
+
+@Component
+public class StringToDepartamentoConversor implements Converter<String, Departamento> {
+
+	@Autowired
+	private DepartamentoService service;
+
+	@Override
+	public Departamento convert(String text) {
+		if(text.isEmpty()) {
+			return null;
+		}
+		Long id = Long.valueOf(text);
+		return service.buscarPorId(id);
+	}
+
+}
